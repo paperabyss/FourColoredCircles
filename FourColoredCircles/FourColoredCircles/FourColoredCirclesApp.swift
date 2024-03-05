d90be00c9757f946158c3551b5cecc9057747813
paperabyss <103468074+paperabyss@users.noreply.github.com>
2024-03-06 08:43:53 +0900

    Adding xcode project

---
+++ FourColoredCircles/FourColoredCircles/FourColoredCirclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FourColoredCirclesApp.swift
+//  FourColoredCircles
+//
+//  Created by Tanner King on 3/6/24.
+//
+
+import SwiftUI
+
+@main
+struct FourColoredCirclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
